96ac7ae42144d772c2b65a818ff4dd3eba886b25
Ken Chigozie <kenronkwo@gmail.com>
2024-05-07 09:50:10 +0100

    portfolio

--- src/theme.js
+++ src/theme.js
@@ -10,7 +10,7 @@ export const blueTheme = {
   compImgHighlight: "#E6E6E6",
   jacketColor: "#0A2472",
   headerColor: "#0E6BA877",
-  splashBg: "#001C55",
+  splashBg: "#0000",
 };
 
 export const brownTheme = {

92a2c02e9a7b5741e6f2b49fc43c4e7c63156146
Ken Chigozie <kenronkwo@gmail.com>
2024-05-07 10:06:08 +0100

    portfolio

--- src/components/Loader/LoaderLogo.js
+++ src/components/Loader/LoaderLogo.js
@@ -5,7 +5,7 @@ import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 class LogoLoader extends React.Component {
   render() {
     const theme = this.props.theme;
-    return <ClimbingBoxLoader size={15} color="#36d7b7" />;
+    return <ClimbingBoxLoader size={12} color="#36d7b7" />;
   }
 }
 
